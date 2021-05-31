0e7ebfff72f6b0a1fa6242d5e1183e9cf100478c
DarsheelParikh <76078295+DarsheelParikh@users.noreply.github.com>
2021-05-31 12:33:49 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function preload(){
+spaceshipImg = loadImage("rocketship.png")
+enemyshipImg = loadImage("enemyship.png")
+backgroundImg = loadImage("spacebackground.png")
+}
+function setup() {
+  createCanvas(1000,1000);
+  background = createSprite(400,200,1000,1000)
+  background.addImage(backgroundImg)
+  background.scale = 3
+spaceship = createSprite(450,600,20,20)
+spaceship.addImage(spaceshipImg)
+spaceship.scale = 1
+enemyship = createSprite(450,150,20,20)
+enemyship.addImage(enemyshipImg)
+enemyship.scale = 1
+}
+
+function draw() {
+  drawSprites();
+}
